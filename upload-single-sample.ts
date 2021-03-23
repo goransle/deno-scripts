6b8a6c560d144cbf7e432371806cfcbd85608262
Gøran A. Slettemark <goransle@gmail.com>
2021-03-23 13:32:53 +0100

    Initial script

---
+++ upload-single-sample.ts
@@ -0,0 +1,39 @@
+import { parse } from "https://deno.land/std/flags/mod.ts";
+import { join } from "https://deno.land/std/path/posix.ts";
+const { args, stdout, run } = Deno;
+
+const parsedArgs = parse(args);
+const requiredArgs = ['path', 'sample'];
+
+if (!requiredArgs.every(arg => parsedArgs[arg])) {
+    throw new Error('Missing one or more of required arguments: '+ requiredArgs.toString());
+}
+
+const { path, sample } = parsedArgs;
+const commands = ['', 'nonav', 'embed'].map(sampleType => {
+    const relativeLocation = sample.replace('samples/', `samples/${sampleType}/`)
+    return run({
+        cmd: [
+            'node',
+            './bin/cli.js',
+            'demo-deploy',
+            '-input',
+            join(path, relativeLocation),
+            '-bucket',
+            'assets.highcharts.com',
+            '-output',
+            relativeLocation,
+            '-make-redirects'
+        ],
+        stdout: 'piped'
+    });
+});
+
+for await (const cmd of commands) {
+    const { code } = await cmd.status()
+    if (code === 0) {
+        const rawOutput = await cmd.output();
+        await stdout.write(rawOutput);
+    }
+    cmd.close();
+}
