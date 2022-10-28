787d4c233a1d8d31da053fd2771cc21f7517b05f
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2022-10-28 09:00:34 +0200

    fixed and simplified upload script

--- upload-single-sample.ts
+++ upload-single-sample.ts
@@ -1,44 +1,43 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { join } from "https://deno.land/std/path/posix.ts";
-const { args, stdout, run } = Deno;
+import { run } from "https://deno.land/x/run_simple/mod.ts";
 
+const { args } = Deno;
 const parsedArgs = parse(args);
-const requiredArgs = ['path', 'sample'];
+const requiredArgs = ["path", "sample"];
 
-if (!requiredArgs.every(arg => parsedArgs[arg])) {
-    throw new Error('Missing one or more of required arguments: '+ requiredArgs.toString());
+if (!requiredArgs.every((arg) => parsedArgs[arg])) {
+  throw new Error(
+    "Missing one or more of required arguments: " + requiredArgs.toString(),
+  );
 }
 
-const variants: string[] = parsedArgs.variants.split(',') || ['', 'nonav', 'embed'];
+const variants: string[] = parsedArgs.variants.split(",") ||
+  ["", "nonav", "embed"];
 
 const { path, sample } = parsedArgs;
-const commands = variants.map(sampleType => {
-    const relativeLocation = sample.replace('samples/', `samples/${sampleType}/`);
+const commands = variants.map(async (sampleType) => {
+  const relativeLocation = sample.replace("samples/", `samples/${sampleType}/`);
+  console.log(relativeLocation);
+  const output = join("demos/samples/", relativeLocation);
 
-    return run({
-        cmd: [
-            'node',
-            './bin/cli.js',
-            'demo-deploy',
-            '-input',
-            join(path, relativeLocation),
-            '-bucket',
-            'assets.highcharts.com',
-            '-output',
-            join('demos', relativeLocation),
-            '-make-redirects',
-            '-AWSProfile',
-            'default'
-        ],
-        stdout: 'piped'
-    });
+  console.log(output);
+
+  const stdout = await run([
+    "node",
+    "./bin/cli.js",
+    "demo-deploy",
+    "-input",
+    join(path, relativeLocation),
+    "-bucket",
+    "assets.highcharts.com",
+    "-output",
+    output,
+    "-make-redirects",
+    "-AWSProfile",
+    "default"
+  ]);
+  console.log(stdout);
 });
 
-for await (const cmd of commands) {
-    const { code } = await cmd.status()
-    if (code === 0) {
-        const rawOutput = await cmd.output();
-        await stdout.write(rawOutput);
-    }
-    cmd.close();
-}
+await Promise.all(commands);
